f82f92ce0af7b2238feb2cd8c5e94029f32c2094
CJG <chjugomez@utp.edu.co>
2021-07-13 15:45:12 -0500

    immproved logic to bring characters info

--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CharacterService } from 'src/app/services/character.service';
 
 @Component({
   selector: 'app-character-list',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterListComponent implements OnInit {
 
-  constructor() { }
+  public charactersAmount : number;
+  public charactersWebsites : any;
+  public charactersInfo : any;
+  public characters : Array<any>;
+  public first : number;
 
-  ngOnInit(): void {
+  constructor(
+    private characterService : CharacterService
+    ) { 
+    this.first = 0;
+    this.charactersAmount = 0;
+    this.charactersWebsites = {};
+    this.charactersInfo = {};
+    this.characters = new Array<any>();
   }
 
+  ngOnInit(): void {
+    this.getCharactersWebsites();
+  }
+
+  private getCharactersWebsites() : void {
+    this.characterService.getCharactersWebsites().subscribe(
+      charactersWebsites => {
+        this.charactersWebsites = charactersWebsites;
+        this.getCharactersInfo();
+      },
+      error => console.error(error)
+    );
+  }
+
+  private getCharactersInfo() : void {
+    this.characterService.getCharactersInfo(<string> this.charactersWebsites.characters).subscribe(
+      charactersInfo => {
+        this.charactersInfo = charactersInfo;
+        this.saveCharacters();
+      },
+      error => console.error(error)
+    )
+  }
+
+  private saveCharacters() : void {
+    this.characters = this.charactersInfo.results;
+    this.charactersAmount = this.characters.length;
+  }
+
+  public onPageChange(event : any) : void {
+    this.first = event.first;
+  }
 }
