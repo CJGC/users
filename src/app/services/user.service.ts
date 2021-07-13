8cb5ae59b53a39f938dff26f52666778d9e3b405
CJG <chjugomez@utp.edu.co>
2021-07-13 09:34:52 -0500

    implemented user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserDto } from '../DTO/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  public getUsers() : Observable<Array<UserDto>> {
+    return this.http.get<Array<UserDto>>(environment.apiURL + '/api/user');
+  }
+
+  public saveStudent(user : UserDto) : Observable<UserDto> {
+    return this.http.post<UserDto>(environment.apiURL, user);
+  }
 }
