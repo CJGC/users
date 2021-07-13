b0a30d7ed31dc87485087f648c9c1cfe96472c49
CJG <chjugomez@utp.edu.co>
2021-07-13 12:20:45 -0500

    added characters route for character list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterListComponent } from './components/character-list/character-list.component';
 import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
   { path: 'users', component: UsersComponent},
+  { path: 'characters', component: CharacterListComponent},
   { path: '', redirectTo: 'users', pathMatch: 'full'}
 ];
 
